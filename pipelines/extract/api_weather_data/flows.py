074963b5065a14553a14595d2f68ccf36c35effb
Daniel Lira <danielllira@icloud.com>
2026-04-19 16:45:26 -0300

    chore: testing

--- pipelines/extract/api_weather_data/flows.py
+++ pipelines/extract/api_weather_data/flows.py
@@ -6,5 +6,5 @@ from pipelines.extract.api_weather_data.tasks import extract_weather_data
 @flow(name="Extract - API Weather Data")
 def api_weather_data(env: str = "dev") -> dict:
     data = extract_weather_data()
-    print(f"Running test in env={env}")
+    print(f"Running in env={env}")
     return data
